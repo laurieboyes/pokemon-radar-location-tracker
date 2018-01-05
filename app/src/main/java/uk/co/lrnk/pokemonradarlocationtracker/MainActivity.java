9da3807c2e65e431790e97ef68e8b698f930918a
Laurie Boyes <laurie.boyes@ft.com>
2018-01-05 14:58:17 +0000

    hook up the button

--- app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/MainActivity.java
+++ app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/MainActivity.java
@@ -2,6 +2,7 @@ package uk.co.lrnk.pokemonradarlocationtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void turnOnLocationTracking(View view) {
+        System.out.println("Woohoo! I can still Java!");
     }
 }
