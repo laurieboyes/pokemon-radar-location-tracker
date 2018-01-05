3f4411d0e7db1c4c912e573bb94296a02c7e5152
Laurie Boyes <laurie.boyes@ft.com>
2018-01-05 17:38:15 +0000

    sweet, it was working all along ✨

--- app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/MainActivity.java
+++ app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/MainActivity.java
@@ -2,13 +2,12 @@ package uk.co.lrnk.pokemonradarlocationtracker;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,30 +23,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void turnOnLocationTracking(View view) {
 
-        ThingyReceiver.scheduleAlarms(this);
+        // todo only allow it to run once
 
-        //        System.out.println("Woohoo! I can still Java!");
-//
-//        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-//
-//        Intent trackerIntent = new Intent(this, ThingyReceiver.class);
-//        alarmIntent = PendingIntent.getBroadcast(this, 0,
-//                trackerIntent, 0);
-//
-//        // Hopefully your alarm will have a lower frequency than this!
-////        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-////                SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_HOUR,
-////                AlarmManager.INTERVAL_HOUR, alarmIntent);
-//
-//        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-//                SystemClock.elapsedRealtime() + 1000,
-//                1000, alarmIntent);
+        final int PERIOD = 60 * 1000;
 
+        alarmManager =
+                (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(this, LocationTrackerService.class);
+        alarmIntent = PendingIntent.getService(this, 0, i, 0);
 
+        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + PERIOD, PERIOD, alarmIntent);
     }
 
     public void turnOfLocationTracking(View view) {
-        if(alarmManager != null) {
+        if (alarmManager != null) {
             alarmManager.cancel(alarmIntent);
         }
     }
