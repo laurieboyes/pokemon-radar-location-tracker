d95b6afe3f105718e8663e59e71ec63cad724227
Laurie Boyes <laurie.boyes@ft.com>
2018-01-07 13:55:53 +0000

    text box for uuid, val stored in prefs and retreived on app load

--- app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/LocationTrackerJobService.java
+++ app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/LocationTrackerJobService.java
@@ -2,6 +2,7 @@ package uk.co.lrnk.pokemonradarlocationtracker;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Handler;
 import android.os.Message;
@@ -42,23 +43,33 @@ public class LocationTrackerJobService extends JobService {
         String url = "https://api.myjson.com/bins/fpe57";
 
         try {
-            JSONObject body = new JSONObject("{\"uuid\":\"48abbf1a-e0dd-4a51-850d-61250ef67aeb\",\"location\":{\"lat\":\"" + location.getLatitude() + "\",\"lng\":\"" + location.getLongitude() + "\"}, \"updatedAt\":\"" + new Date().toString() + "\"}");
 
-            // Remember for real URL method must be POST
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, body,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            System.out.println("Response: " + response);
-                            Toast.makeText(getApplicationContext(), "Sent location to Pokémon Radar 📍", Toast.LENGTH_LONG).show();
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    System.out.println("Response error: " + error);
-                }
-            });
-            queue.add(request);
+            SharedPreferences settings = getSharedPreferences(MainActivity.PREFS_NAME, 0);
+            String uuid = settings.getString("uuid", "");
+            if(!uuid.isEmpty()) {
+                JSONObject body = new JSONObject("{\"uuid\":\"" + uuid + "\",\"location\":{\"lat\":\"" + location.getLatitude() + "\",\"lng\":\"" + location.getLongitude() + "\"}, \"updatedAt\":\"" + new Date().toString() + "\"}");
+
+                // Remember for real URL method must be POST
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, body,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                System.out.println("Response: " + response);
+                                Toast.makeText(getApplicationContext(), "Sent location to Pokémon Radar 📍", Toast.LENGTH_LONG).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("Response error: " + error);
+                    }
+                });
+                queue.add(request);
+            } else {
+                System.out.println("No uuid saved");
+                Toast.makeText(getApplicationContext(), "No UUID saved 🤔", Toast.LENGTH_LONG).show();
+            }
+
+
         } catch (JSONException e) {
             System.out.println("Error parsing JSON body: " + e);
         }
--- app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/MainActivity.java
+++ app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/MainActivity.java
@@ -5,29 +5,35 @@ import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-//    public static final String PREFS_NAME = "PokemonRadarLocationTracker";
+    public static final String PREFS_NAME = "PokemonRadarLocationTracker";
+    private SharedPreferences settings;
 
     final int jobId = 890709;
     final int permissionRequestId = 3123;
+    ToggleButton toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ToggleButton toggle = findViewById(R.id.toggleButton);
-        if(isTrackingOn()) {
+        toggle = findViewById(R.id.toggleButton);
+        if (isTrackingOn()) {
             toggle.setChecked(true);
         }
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -37,6 +43,34 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     turnOffTracking();
                 }
+            }
+        });
+
+        settings = getSharedPreferences(PREFS_NAME, 0);
+        final EditText editText = findViewById(R.id.editText);
+        editText.setText(settings.getString("uuid", ""));
+        findViewById(R.id.mainLayout).requestFocus();
+
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                System.out.println(s);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("uuid", s.toString());
+                editor.apply();
+
+                if (isTrackingOn()) {
+                    toggle.setChecked(false);
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
         });
     }
@@ -66,24 +100,29 @@ public class MainActivity extends AppCompatActivity {
     private void scheduleJob() {
         final int PERIOD = 15 * 60 * 1000;
 
-        JobScheduler mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        if (settings.getString("uuid", "").isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter your Pokémon Radar UUID into the text box and try again", Toast.LENGTH_LONG).show();
+            toggle.setChecked(false);
+        } else {
+            JobScheduler mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
 
-        JobInfo.Builder builder = new JobInfo.Builder(jobId,
-                new ComponentName(getPackageName(),
-                        LocationTrackerJobService.class.getName()));
+            JobInfo.Builder builder = new JobInfo.Builder(jobId,
+                    new ComponentName(getPackageName(),
+                            LocationTrackerJobService.class.getName()));
 
-        builder.setPeriodic(PERIOD);
-        int scheduleResult = mJobScheduler.schedule(builder.build());
-        if (scheduleResult == JobScheduler.RESULT_SUCCESS) {
-            Toast.makeText(getApplicationContext(), "Sending location roughly every " + (PERIOD / (60 * 1000)) + " minutes", Toast.LENGTH_LONG).show();
+            builder.setPeriodic(PERIOD);
+            int scheduleResult = mJobScheduler.schedule(builder.build());
+            if (scheduleResult == JobScheduler.RESULT_SUCCESS) {
+                Toast.makeText(getApplicationContext(), "Sending location roughly every " + (PERIOD / (60 * 1000)) + " minutes", Toast.LENGTH_LONG).show();
 
 //            SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 //            SharedPreferences.Editor editor = settings.edit();
 //            editor.putBoolean("trackingOn", true);
 //            editor.apply();
 
-        } else {
-            Toast.makeText(getApplicationContext(), "Error scheduling job 🤷‍♀️", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Error scheduling job 🤷‍♀️", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -102,14 +141,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void turnOffTracking() {
-        JobScheduler mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        mJobScheduler.cancel(jobId);
 
-//        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-//        SharedPreferences.Editor editor = settings.edit();
-//        editor.putBoolean("trackingOn", false);
-//        editor.apply();
+        // This might happen as a result of the trackign being aborted, so check this first
+        if (isTrackingOn()) {
 
-        Toast.makeText(getApplicationContext(), "Turning off location tracking", Toast.LENGTH_LONG).show();
+            //        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            //        SharedPreferences.Editor editor = settings.edit();
+            //        editor.putBoolean("trackingOn", false);
+            //        editor.apply();
+
+            JobScheduler mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+            mJobScheduler.cancel(jobId);
+
+            Toast.makeText(getApplicationContext(), "Turning off location tracking", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
