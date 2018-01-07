0633b3368f31f62d6c832e208d8889c677aa6543
Laurie Boyes <laurie.boyes@ft.com>
2018-01-07 12:08:05 +0000

    real location, including permission request

--- app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/LocationTrackerJobService.java
+++ app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/LocationTrackerJobService.java
@@ -2,6 +2,7 @@ package uk.co.lrnk.pokemonradarlocationtracker;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.location.Location;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.Toast;
@@ -12,6 +13,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,38 +36,58 @@ public class LocationTrackerJobService extends JobService {
         return false;
     }
 
+    private void sendUpdateRequest(Location location) {
+        RequestQueue queue = Volley.newRequestQueue(LocationTrackerJobService.this);
+//        String url = "https://mon-radar.herokuapp.com/api/updateLocation/";
+        String url = "https://api.myjson.com/bins/fpe57";
+
+        try {
+            JSONObject body = new JSONObject("{\"uuid\":\"48abbf1a-e0dd-4a51-850d-61250ef67aeb\",\"location\":{\"lat\":\"" + location.getLatitude() + "\",\"lng\":\"" + location.getLongitude() + "\"}, \"updatedAt\":\"" + new Date().toString() + "\"}");
+
+            // Remember for real URL method must be POST
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, body,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println("Response: " + response);
+                            Toast.makeText(getApplicationContext(), "Sent location to Pokémon Radar 📍", Toast.LENGTH_LONG).show();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    System.out.println("Response error: " + error);
+                }
+            });
+            queue.add(request);
+        } catch (JSONException e) {
+            System.out.println("Error parsing JSON body: " + e);
+        }
+    }
+
     private Handler mJobHandler = new Handler(new Handler.Callback() {
 
         @Override
         public boolean handleMessage(Message msg) {
             System.out.println("Hello from the job handler thing!");
 
-            RequestQueue queue = Volley.newRequestQueue(LocationTrackerJobService.this);
-//        String url = "https://mon-radar.herokuapp.com/api/updateLocation/";
-            String url = "https://api.myjson.com/bins/fpe57";
-
+            FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(LocationTrackerJobService.this);
             try {
-                JSONObject body = new JSONObject("{\"uuid\":\"48abbf1a-e0dd-4a51-850d-61250ef67aeb\",\"location\":{\"lat\":\"1.123\",\"lng\":\"2.345\"}, \"updatedAt\":\"" + new Date().toString() + "\"}");
-
-                // Remember for real URL method must be POST
-                JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, body,
-                        new Response.Listener<JSONObject>() {
+                mFusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(new OnSuccessListener<Location>() {
                             @Override
-                            public void onResponse(JSONObject response) {
-                                System.out.println("Response: " + response);
-                                Toast.makeText(getApplicationContext(), "Sent location to Pokémon Radar 📍", Toast.LENGTH_LONG).show();
+                            public void onSuccess(Location location) {
+                                if (location != null) {
+                                    System.out.println("sick, got location " + location);
+                                    sendUpdateRequest(location);
+                                } else {
+                                    System.out.println("location was null for some reason???");
+                                }
                             }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println("Response error: " + error);
-                    }
-                });
-                queue.add(request);
-            } catch (JSONException e) {
-                System.out.println("Error parsing JSON body: " + e);
+                        });
+            } catch (SecurityException e) {
+                System.out.println("Huh, SecurityException" + e);
+                Toast.makeText(getApplicationContext(), "SecurityException trying to get location", Toast.LENGTH_LONG).show();
             }
-
             jobFinished((JobParameters) msg.obj, false);
             return true;
         }
--- app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/MainActivity.java
+++ app/src/main/java/uk/co/lrnk/pokemonradarlocationtracker/MainActivity.java
@@ -1,9 +1,13 @@
 package uk.co.lrnk.pokemonradarlocationtracker;
 
+import android.Manifest;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +16,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     final int jobId = 890709;
+    final int permissionRequestId = 3123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +24,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case permissionRequestId: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    scheduleJob();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Do try again if you change your mind️", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
 
-    public void turnOnLocationTracking(View view) {
-
+    private void scheduleJob() {
         final int PERIOD = 15 * 60 * 1000;
 
         JobScheduler mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
@@ -36,6 +55,20 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Sending location roughly every " + (PERIOD / (60 * 1000)) + " minutes", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(getApplicationContext(), "Error scheduling job 🤷‍♀️", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void turnOnLocationTracking(View view) {
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    permissionRequestId);
+        } else {
+            scheduleJob();
         }
     }
 
